ef7716e94b2d165c58b9fd2aef264b4cb8aca213
Jess Pennock <jess.pennock@gmail.com>
2022-09-16 08:03:38 -0600

    Completed problems 1-2 about Software Development Lifeccycle

---
+++ sdlc.js
@@ -0,0 +1,27 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+ 
+// Planning 
+// Analysis of Requirements 
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirements = "This is the second step where you define an applications requirements and what it is supposed to do. Requirements also include defining the resources required to complete the project."
+const design = "In the design phase you create a model of how the software application will work. Some aspects defined here might include: UI, Architecture, Plaforms, Programming, Security, and Communications. Prototyping might also be a part of this phase."
+const implementation = "This is where the software development happens and the program is actually being written."
+const testingandIntegration = "Applications should be tested before they are passed on to users. Testing your code will reduce the number of bugs and problems your users are faced with. This should lead to a higher user satisfaction with your application. Once testing is complete, the software is deployed, or made available to users."
+const maintain = "This the final step of the software development lifecycle. Here developers keep things running smoothly. This might include fixing previously undiscovered bugs and updating software requirements."
